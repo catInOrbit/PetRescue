59149e0241bd8392fa462c3707274b6e96b3ef51
Huy <quanghuy15071999@gmail.com>
2020-10-15 18:29:52 +0700

    Update profile screen\

--- lib/EditScreen.dart
+++ lib/EditScreen.dart
@@ -9,6 +9,83 @@ class EditScreen extends StatefulWidget {
 class _EditScreenState extends State<EditScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: HexColor('#FFB9AC'),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: HexColor('#FFB9AC'),
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () => Navigator.pop(context, false),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              color: Colors.white,
+              child: Center(
+                child: CircleAvatar(
+                  radius: 40,
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Column(
+                children: <Widget>[
+                  Text("Basic Information", style: TextStyle(
+                    fontSize: 20, fontWeight: FontWeight.bold
+                  ),),
+                  SizedBox(height: 5,),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Your name",
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
