28299928e18aa193a67d954176b4460f31e028fb
Huy <quanghuy15071999@gmail.com>
2020-10-15 12:38:29 +0700

    new profileScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:petrescue/Colors.dart';
+import 'package:petrescue/SilverAppBar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: profildsab(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,107 +15,197 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ProfileScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ProfileScreenState createState() => _ProfileScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: HexColor('#FFB9AC'),
+        appBar: AppBar(
+          backgroundColor: HexColor('#FFB9AC'),
+          elevation: 0,
+          automaticallyImplyLeading: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () => Navigator.pop(context, false),
+          ),
+          title: Text(
+            "My Profile",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          actions: <Widget>[
+            SizedBox(
+              width: 40.0,
+              child: IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {},
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
             ),
           ],
         ),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: Container(
+                //margin: EdgeInsets.all(10),
+                height: 230,
+                width: MediaQuery.of(context).size.width * 0.7,
+                // decoration: BoxDecoration(color: Colors.white),
+                child: Container(
+                  //margin: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 40,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Full Name",
+                        style: TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.business_center,
+                            size: 13,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text("Job", style: TextStyle(fontSize: 13)),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text("|"),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text("ABC Center", style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.phone,
+                            size: 13,
+                          ),
+                          Text("1234567890", style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.location_on,
+                            size: 13,
+                          ),
+                          Text("123 abc street",
+                              style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.email,
+                            size: 13,
+                          ),
+                          Text("asdfasfsadf@gmail.com",
+                              style: TextStyle(fontSize: 13)),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text("Website", style: TextStyle(fontSize: 13)),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              child: AppBar(
+                bottom: TabBar(
+                  tabs: [
+                    Tab(
+                      icon: Icon(Icons.directions_bike),
+                    ),
+                    Tab(
+                      icon: Icon(
+                        Icons.directions_car,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: TabBarView(
+                children: [
+                // first tab bar view widget
+                Container(
+                color: Colors.red,
+                child: Center(
+                  child: Text(
+                    'Bike',
+                  ),
+                ),
+              ),
+// second tab bar viiew widget
+                  Container(
+                    color: Colors.pink,
+                    child: Center(
+                      child: Text(
+                        'Car',
+                      ),
+                    ),
+                  ),
+                ],
+            ))
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
