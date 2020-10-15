0ebc4195650814c9f8fe812554f13f27c7f73c9c
Huy <quanghuy15071999@gmail.com>
2020-10-15 13:27:43 +0700

    update profileScreen scrollable

---
+++ lib/EditScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:petrescue/Colors.dart';
+
+class EditScreen extends StatefulWidget {
+  @override
+  _EditScreenState createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
