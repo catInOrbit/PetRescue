a70f376f0f31a73ad2ef7fa15e9780ad01312e67
Huy <quanghuy15071999@gmail.com>
2020-10-19 22:42:46 +0700

    createpost and second createpost

---
+++ lib/Post2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:petrescue/Colors.dart';
+
+class PostSecond extends StatefulWidget {
+  @override
+  _PostSecondState createState() => _PostSecondState();
+}
+
+class _PostSecondState extends State<PostSecond> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
