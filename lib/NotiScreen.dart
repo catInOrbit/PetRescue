e3d2e3ae797194cca18f3b417ee1866cfbf9cb4a
Huy <quanghuy15071999@gmail.com>
2020-10-20 12:53:54 +0700

    create noti screen

---
+++ lib/NotiScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotiScreen extends StatefulWidget {
+  @override
+  _NotiScreenState createState() => _NotiScreenState();
+}
+
+class _NotiScreenState extends State<NotiScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
