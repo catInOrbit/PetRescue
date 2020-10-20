770cc77ed1d265ffe909a578fe48c78ec29206ec
catInOrbit <magnetocat01@gmail.com>
2020-10-20 15:36:48 +0700

    notification screen

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,16 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:petrescue/petrescue_theme.dart';
 import 'package:petrescue/widgets/notification_widget.dart';
 
 class NotificationScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Column(
+    return DefaultTabController(
+      length: 2,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: PetRescueTheme.lightPink,
+          appBar: AppBar(
+            title: Text("Messengers"),
+            centerTitle: true,
+            bottom: TabBar(
+              // isScrollable: true,
+              //controller: tabController,
+              tabs: <Widget>[
+                Tab(text: "Chats"),
+                Tab(
+                  text: 'Notifications',
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
             children: [
-              NotificationWidget()
-            ],
+              Center(
+                child: Column(
+                  children: [
+                    NotificationWidget(),
+                    NotificationWidget(),
+
+                  ],
+                ),
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    NotificationWidget()
+                  ],
+                ),
+              ),
+            ]
           ),
         ),
       ),
--- lib/widgets/notification_widget.dart
+++ lib/widgets/notification_widget.dart
@@ -3,93 +3,165 @@ import 'package:flutter/material.dart';
 class NotificationWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "August 21, 14:57PM",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontStyle: FontStyle.italic,
-              fontFamily: "Roboto",
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-          SizedBox(height: 6),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(13),
-              color: Color(0xffabd2bf),
-            ),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 12,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 5,
-                        bottom: 9,
-                      ),
-                      child: Opacity(
-                        opacity: 0.50,
-                        child: Container(
-                          width: 45,
-                          height: 40.50,
-                          color: Color(0xff4b8669),
-                        ),
+    return Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Container(
+          width: 350,
+          height: 127,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(28),
+                      border: Border.all(
+                        color: Colors.white,
+                        width: 2,
                       ),
                     ),
-                    SizedBox(width: 18),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          width: 256,
-                          height: 29,
-                          child: Text(
-                            "Request accepted",
-                            style: TextStyle(
-                              color: Color(0xff4b8669),
-                              fontSize: 20,
-                              fontFamily: "Roboto",
-                              fontWeight: FontWeight.w900,
-                            ),
+                    padding: const EdgeInsets.only(
+                      left: 9,
+                      right: 8,
+                      top: 2,
+                      bottom: 3,
+                    ),
+                    child: Text(
+                      "08/21/2020",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontStyle: FontStyle.italic,
+                        fontFamily: "Roboto",
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 315,
+                top: 11,
+                child: Text(
+                  "14:52",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 11,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28),
+                          border: Border.all(
+                            color: Colors.white,
+                            width: 2,
                           ),
                         ),
-                        SizedBox(height: 10),
-                        Text(
-                          "User accepted your adoption request",
+                        padding: const EdgeInsets.only(
+                          left: 9,
+                          right: 8,
+                          top: 2,
+                          bottom: 3,
+                        ),
+                        child: Text(
+                          "08/21/2020",
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 16,
+                            fontSize: 18,
+                            fontStyle: FontStyle.italic,
                             fontFamily: "Roboto",
-                            fontWeight: FontWeight.w300,
+                            fontWeight: FontWeight.w400,
                           ),
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      SizedBox(height: 3),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(13),
+                          color: Colors.white,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 12,
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                    top: 5,
+                                    bottom: 9,
+                                  ),
+                                  child: Opacity(
+                                    opacity: 0.50,
+                                    child: Container(
+                                      width: 45,
+                                      height: 40.50,
+                                      color: Color(0xff4b8669),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 18),
+                                Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      width: 256,
+                                      height: 29,
+                                      child: Text(
+                                        "Request accepted",
+                                        style: TextStyle(
+                                          color: Color(0xff4b8669),
+                                          fontSize: 20,
+                                          fontFamily: "Roboto",
+                                          fontWeight: FontWeight.w900,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      "User accepted your adoption request",
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 16,
+                                        fontFamily: "Roboto",
+                                        fontWeight: FontWeight.w300,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Container(
+                              width: 18,
+                              height: 18,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                Container(
-                  width: 18,
-                  height: 18,
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );
+        ));
   }
 }
