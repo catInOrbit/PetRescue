ac5d227309434b0966d245b5409cd5df2dac3127
Huy <quanghuy15071999@gmail.com>
2020-10-20 14:37:32 +0700

    create noti screen

--- lib/NotiScreen.dart
+++ lib/NotiScreen.dart
@@ -6,8 +6,46 @@ class NotiScreen extends StatefulWidget {
 }
 
 class _NotiScreenState extends State<NotiScreen> {
+  TabController tabController;
+
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   tabController = TabController(vsync: this, length: 2);
+  // }
+  //
+  // @override
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   tabController.dispose();
+  //   super.dispose();
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("Messengers"),
+          centerTitle: true,
+          bottom: TabBar(
+            // isScrollable: true,
+            //controller: tabController,
+            tabs: <Widget>[
+              Tab(text: "Chats"),
+              Tab(
+                text: 'Notifications',
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+
+        ),
+      ),
+    );
   }
 }
