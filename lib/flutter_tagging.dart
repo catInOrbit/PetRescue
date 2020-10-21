945f3ea08cb582ef2005d93007ea9c6f8852c1bf
Huy <quanghuy15071999@gmail.com>
2020-10-21 23:18:42 +0700

    update post2

---
+++ lib/flutter_tagging.dart
@@ -0,0 +1,11 @@
+// Copyright 2020 Sarbagya Dhaubanjar. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+library flutter_tagging;
+
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+
+export 'FlutterTagging/configurations.dart';
+export 'FlutterTagging/taggable.dart';
+export 'FlutterTagging/tagging.dart';
