8ac109e3f4ab9f343ed4c54043126140fd9190e1
Huy <quanghuy15071999@gmail.com>
2020-11-02 03:02:10 +0700

    update   3:00 3/11

--- lib/profile/TabAdoption.dart
+++ lib/profile/TabAdoption.dart
@@ -11,98 +11,27 @@ class _PageAdoptionState extends State<PageAdoption> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.only(top:10.0),
+        padding: const EdgeInsets.only(top: 10.0),
         child: Column(
           children: <Widget>[
-            // GestureDetector(
-            //   onTap: (){},
-            //   child: Container(
-            //     width: MediaQuery.of(context).size.width,
-            //     height: 140,
-            //     padding: EdgeInsets.fromLTRB(10.0,0,10,5),
-            //     // decoration: BoxDecoration(
-            //     //   border: Border(
-            //     //     bottom: BorderSide(
-            //     //       width: 0.5
-            //     //     )
-            //     //   )
-            //     // ),
-            //     child: Material(
-            //       elevation: 4.0,
-            //       borderRadius: BorderRadius.circular(5.0),
-            //
-            //       child:Row(
-            //         children: <Widget>[
-            //           Padding(
-            //             padding: const EdgeInsets.fromLTRB(5,0,10,0),
-            //             child: CircleAvatar(
-            //               radius: 50,
-            //             ),
-            //           ),
-            //           Expanded(
-            //             child: Container(
-            //               // height: 100,
-            //               // width: 245,
-            //               // color: Colors.red,
-            //               child: Column(
-            //                 children: <Widget>[
-            //                   Align(
-            //                     alignment: Alignment.topLeft,
-            //                     child: Text(
-            //                       "Mao Trach Dong",
-            //                       style: TextStyle(
-            //                         color: Color(0xff4b8669),
-            //                         fontSize: 20,
-            //                         fontFamily: "Roboto",
-            //                         fontWeight: FontWeight.w900,
-            //                       ),
-            //                     ),
-            //                   ),
-            //                   Align(
-            //                       alignment: Alignment.centerLeft,
-            //                       child: Text("Dog")),
-            //                   SizedBox(height: 5,),
-            //                   Align(
-            //                     alignment: Alignment.centerLeft,
-            //                     child: Text(
-            //                       "Has been raised for 2 months",
-            //                       style: TextStyle(
-            //                         color: Colors.black,
-            //                         fontSize: 15,
-            //                         fontFamily: "Roboto",
-            //                         fontWeight: FontWeight.w300,
-            //                       ),
-            //                     ),
-            //                   ),
-            //                   SizedBox(height: 5,),
-            //                   Align(
-            //                     alignment: Alignment.centerRight,
-            //                     child: Row(
-            //                       mainAxisSize: MainAxisSize.min,
-            //                       children: <Widget>[
-            //                         Text("Ha Noi"),
-            //                         Icon(Icons.location_on)
-            //                       ],
-            //                     ),
-            //                   )
-            //                 ],
-            //               ),
-            //             ),
-            //           ),
-            //
-            //         ],
-            //       ),
-            //     ),
-            //   ),
-            // ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 PetAdoptionWidget(),
-                SizedBox(width: 20,),
+                SizedBox(
+                  width: 20,
+                ),
                 PetAdoptionWidget(),
               ],
             ),
+            SizedBox(height: 10,),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              PetAdoption2(),
+              SizedBox(
+                width: 20,
+              ),
+              PetAdoption2(),
+            ])
           ],
         ),
       ),
@@ -120,42 +49,44 @@ class PetAdoptionWidget extends StatelessWidget {
         width: MediaQuery.of(context).size.width * 0.45,
         height: 280,
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('lib/assets/Cat2.png'),
-            fit: BoxFit.cover
-          ),
-          borderRadius: BorderRadius.circular(10.0)
-        ),
+            image: DecorationImage(
+                image: AssetImage('lib/assets/Cat2.png'), fit: BoxFit.cover),
+            borderRadius: BorderRadius.circular(10.0)),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
-
             children: <Widget>[
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                Text("Miu Miu", style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
-                ),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,),
-                SizedBox(width: 10,),
-                Text("Male",style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 13
-                ),)
-              ],),
+                  Text(
+                    "Miu Miu",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Cái",
+                    style: TextStyle(color: Colors.white, fontSize: 13),
+                  )
+                ],
+              ),
 
-              SizedBox(height: 5,),
+              SizedBox(
+                height: 5,
+              ),
               Container(
                 alignment: Alignment.topLeft,
                 child: Text(
-                    "Special Dog English, Husky, beautiful, white, adfsdf",
-                  style: TextStyle(
-                      fontSize: 12, color: Colors.white),
+                  "Mèo con. Lông vàng trắnng",
+                  style: TextStyle(fontSize: 12, color: Colors.white),
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                 ),
@@ -163,37 +94,40 @@ class PetAdoptionWidget extends StatelessWidget {
               Container(
                 alignment: Alignment.topLeft,
                 child: Text(
-                  "9 months age",
-                  style: TextStyle(
-                      fontSize: 12, color: Colors.white),
+                  "9 tháng tuổi",
+                  style: TextStyle(fontSize: 12, color: Colors.white),
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                 ),
               ),
-             // SizedBox(height: 5,),
+              // SizedBox(height: 5,),
               Container(
                 alignment: Alignment.topLeft,
                 child: Text(
-                  "Has been raised for 2 months",
-                  style: TextStyle(
-                      fontSize: 12, color: Colors.white),
+                  "Được trung tâm nuôi 2 tháng.",
+                  style: TextStyle(fontSize: 12, color: Colors.white),
                   overflow: TextOverflow.ellipsis,
                   maxLines: 2,
                 ),
-
               ),
 
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text("Ha Noi",
-            style: TextStyle(
-                fontSize: 12, color: Colors.white),),
-                Icon(Icons.location_on, color: Colors.white, size: 12,)
-              ],
-            ),)
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "TP.HCM",
+                      style: TextStyle(fontSize: 12, color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.white,
+                      size: 12,
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
@@ -202,4 +136,113 @@ class PetAdoptionWidget extends StatelessWidget {
   }
 }
 
+class PetAdoption2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        //alignment: Alignment.topLeft,
+        width: MediaQuery.of(context).size.width * 0.45,
+        height: 280,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('lib/assets/Cat2.png'), fit: BoxFit.cover),
+            borderRadius: BorderRadius.circular(10.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Bưởi",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "Đực",
+                    style: TextStyle(color: Colors.white, fontSize: 13),
+                  )
+                ],
+              ),
 
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Chó nhà lông vàng, tai nhọn, chân dài.",
+                  style: TextStyle(fontSize: 12, color: Colors.white),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "1 tuổi rưỡi",
+                  style: TextStyle(fontSize: 12, color: Colors.white),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+              // SizedBox(height: 5,),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Được trung tâm nuôi 1 năm.",
+                  style: TextStyle(fontSize: 12, color: Colors.white),
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 2,
+                ),
+              ),
+
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "TP.HCM",
+                      style: TextStyle(fontSize: 12, color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.white,
+                      size: 12,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PetAdoption3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class PetAdoption4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
